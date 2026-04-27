53363b61a1ddbb35cd218352cbc8eb6c37b8e309
ahmedfamous <ahmedfamous032@gmail.com>
2026-04-28 01:07:20 +0300

    Init

---
+++ routes/userRoute.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const {
+  getUserValidator,
+  createUserValidator,
+  updateUserValidator,
+  deleteUserValidator,
+  changeUserPasswordValidator,
+  updateLoggedUserValidator,
+} = require("../utils/validators/userValidator");
+
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  uploadUserImage,
+  resizeImage,
+  changeUserPassword,
+  getLoggedUserData,
+  updateLoggedUserPassword,
+  updateLoggedUserData,
+  deleteLoggedUserData,
+  updateUserRole,   
+} = require("../services/userService");
+
+
+const authService = require("../services/authService");
+
+const router = express.Router();
+
+router.use(authService.protect);
+
+router.get("/getMe", getLoggedUserData, getUser);
+router.put("/changeMyPassword", updateLoggedUserPassword);
+router.put("/updateMe", updateLoggedUserValidator, updateLoggedUserData);
+router.delete("/deleteMe", deleteLoggedUserData);
+router.patch("/:id/role", authService.allowedTo("admin"), updateUserRole);
+
+
+// Admin
+router.use(authService.allowedTo("admin", "manager"));
+router.put(
+  "/changePassword/:id",
+  changeUserPasswordValidator,
+  changeUserPassword
+);
+router
+  .route("/")
+  .get(getUsers)
+  .post(uploadUserImage, resizeImage, createUserValidator, createUser);
+router
+  .route("/:id")
+  .get(getUserValidator, getUser)
+  .put(uploadUserImage, resizeImage, updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
+
+module.exports = router;
---
+++ services/orderService.js
@@ -0,0 +1,134 @@
+const asyncHandler = require('express-async-handler');
+const factory = require('./handlersFactory');
+const ApiError = require('../utils/apiError');
+
+const User = require('../models/userModel');
+const Product = require('../models/productModel');
+const Cart = require('../models/cartModel');
+const Order = require('../models/orderModel');
+
+// @desc    create cash order
+// @route   POST /api/v1/orders/cartId
+// @access  Protected/User
+exports.createCashOrder = asyncHandler(async (req, res, next) => {
+  // app settings
+  const taxPrice = 0;
+  const shippingPrice = 0;
+
+  // 1) Get cart depend on cartId
+  const cart = await Cart.findById(req.params.cartId);
+  if (!cart) {
+    return next(
+      new ApiError(`There is no such cart with id ${req.params.cartId}`, 404)
+    );
+  }
+
+  // 2) Recalculate total price based on actual product prices
+  let totalPrice = 0;
+
+  for (const item of cart.cartItems) {
+    const product = await Product.findById(item.product);
+    if (!product) {
+      return next(new ApiError(`Product with id ${item.product} not found`, 404));
+    }
+
+    const finalPrice = product.priceAfterDiscount ? product.priceAfterDiscount : product.price;
+
+    item.price = finalPrice; // تحديث السعر جوه السلة حسب السعر الحالي للمنتج
+    totalPrice += finalPrice * item.quantity;
+  }
+
+  cart.totalCartPrice = totalPrice;
+  await cart.save();
+
+  // 3) Get order price depend on cart price "Check if coupon apply"
+  const cartPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount
+    : cart.totalCartPrice;
+
+  const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+
+  // 4) Create order with default paymentMethodType cash
+  const order = await Order.create({
+    user: req.user._id,
+    cartItems: cart.cartItems,
+    shippingAddress: req.body.shippingAddress,
+    totalOrderPrice,
+  });
+
+  // 5) After creating order, decrement product quantity, increment product sold
+  if (order) {
+    const bulkOption = cart.cartItems.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product },
+        update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+      },
+    }));
+    await Product.bulkWrite(bulkOption, {});
+
+    // 6) Clear cart depend on cartId
+    await Cart.findByIdAndDelete(req.params.cartId);
+  }
+
+  res.status(201).json({ status: 'success', data: order });
+});
+
+exports.filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
+  if (req.user.role === 'user') req.filterObj = { user: req.user._id };
+  next();
+});
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
+exports.findAllOrders = factory.getAll(Order);
+
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
+exports.findSpecificOrder = factory.getOne(Order);
+
+// @desc    Update order paid status to paid
+// @route   PUT /api/v1/orders/:id/pay
+// @access  Protected/Admin-Manager
+exports.updateOrderToPaid = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ApiError(
+        `There is no such a order with this id:${req.params.id}`,
+        404
+      )
+    );
+  }
+
+  // update order to paid
+  order.isPaid = true;
+  order.paidAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json({ status: 'success', data: updatedOrder });
+});
+
+// @desc    Update order delivered status
+// @route   PUT /api/v1/orders/:id/deliver
+// @access  Protected/Admin-Manager
+exports.updateOrderToDelivered = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (!order) {
+    return next(
+      new ApiError(
+        `There is no such a order with this id:${req.params.id}`,
+        404
+      )
+    );
+  }
+
+  // update order to paid
+  order.isDelivered = true;
+  order.deliveredAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json({ status: 'success', data: updatedOrder });
+});
